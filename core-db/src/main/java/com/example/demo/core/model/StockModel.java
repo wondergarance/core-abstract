652b2e4e18943ad290a574467b96aea8054f2300
Qian QIAO <q.qiao@dalloz.fr>
2022-04-29 17:20:40 +0200

    Configuration of database

--- core-db/src/main/java/com/example/demo/core/model/StockModel.java
+++ core-db/src/main/java/com/example/demo/core/model/StockModel.java
@@ -1,19 +1,46 @@
 package com.example.demo.core.model;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.*;
+import java.util.Arrays;
 import java.util.List;
 
-@Entity
+@Entity(name = "StockModel")
+@Table(name = "stock")
 public class StockModel {
-    private List<ShoeModel> shoes;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String shoes;
+
+    @Column
     private int capacity;
 
-    public List<ShoeModel> getShoes() {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getShoesJson() {
         return shoes;
     }
 
-    public void setShoes(List<ShoeModel> shoes) {
-        this.shoes = shoes;
+    public List<ShoeModel> getShoes() throws JsonProcessingException {
+        var shoes = objectMapper.readValue(this.shoes, ShoeModel[].class);
+        return Arrays.asList(shoes);
+    }
+
+    public void setShoesJson(List<ShoeModel> shoeList) throws JsonProcessingException {
+        this.shoes = objectMapper.writeValueAsString(shoeList);
     }
 
     public int getCapacity() {
---
+++ core-db/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO STOCK (id, shoes, capacity) VALUES (1, '[{"color": "BLACK","size": 40,"quantity": 10},{"color": "BLACK","size": 41,"quantity": 0},{"color": "BLUE","size": 39,"quantity": 10}]', 30);
